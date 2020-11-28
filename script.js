0c152712197d699b9d3b282059907aa1a3291413
Thalen Abadia <thalenabadia@gmail.com>
2020-11-28 21:53:51 +0000

    Hovering function

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function sub()
 {
- var an=document.getElementById('AssignmentName')
- window.alert(an)
+ var an = document.getElementById('AssignmentName').value;
+ var dd = document.getElementById('DueDate').value;
+ var al = document.getElementById('AssignmentLink').value;
+ var ac = document.getElementById('AssignmentClass').value;
+ var td= document.getElementById('ToDo').innerText;
+ var table = document.getElementById("table");
+
+var rows = document.getElementById("table").rows.length; 
+var row = table.insertRow(rows);
+
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+var cell3 = row.insertCell(2);
+
+cell1.innerHTML = "<div class=\"myDIV\><a href=" + al + " target=\"_blank\"></div>"+ an + "</a>";
+cell2.innerHTML = an; 
+cell3.innerHTML = an; 
 }
 
