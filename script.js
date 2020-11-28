5c8850ca0c4c101cf151f6320e9d7712674d285e
Owen Schafer <owcschafer@hotmail.com>
2020-11-28 23:26:53 +0000

    removed tests and added dropdowns

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function sub()
  var dd = document.getElementById('DueDate').value;
  var al = document.getElementById('AssignmentLink').value;
  var ac = document.getElementById('AssignmentClass').value;
- var td= document.getElementById('ToDo').innerText;
+ var td = document.getElementById('dropdown').value;
 
 if (an == null){
   alert("Assignment Name is required")
@@ -14,11 +14,26 @@ var table = document.getElementById("table");
 var rows = table.rows.length; 
 var row = table.insertRow(rows);
 
+if (td == "To Do") {
+  var cell1 = row.insertCell(0);
+  cell1.innerHTML = "<div class=\"myDIV\"><a href=" + al + " target=\"_blank\">" + an + "</a></div> <div class=\"hide\">" + dd + " - " + ac + "</div>";
+}
+else if (td == "Doing") {
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell2.innerHTML = "<div class=\"myDIV\"><a href=" + al + " target=\"_blank\">" + an + "</a></div> <div class=\"hide\">" + dd + " - " + ac + "</div>"; 
+}
+else {
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell3.innerHTML = "<div class=\"myDIV\"><a href=" + al + " target=\"_blank\">" + an + "</a></div> <div class=\"hide\">" + dd + " - " + ac + "</div>"; 
+}
+}
+
 var cell1 = row.insertCell(0);
 var cell2 = row.insertCell(1);
 var cell3 = row.insertCell(2);
 
-cell1.innerHTML = an;
-cell2.innerHTML = "<div class=\"myDIV\"><a href=" + al + " target=\"_blank\">" + an + "</a></div> <div class=\"hide\">" + dd + " - " + ac + "</div>"; 
-cell3.innerHTML = an; 
-}
+
+
