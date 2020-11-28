3aa924671678186b4d859ee7042e126273b809b4
Owen Schafer <owcschafer@hotmail.com>
2020-11-28 22:23:34 +0000

    ok

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ function sub()
  var al = document.getElementById('AssignmentLink').value;
  var ac = document.getElementById('AssignmentClass').value;
  var td= document.getElementById('ToDo').innerText;
- var table = document.getElementById("table");
 
-var rows = document.getElementById("table").rows.length; 
+if (an == null){
+  alert("Assignment Name is required")
+}
+
+var table = document.getElementById("table");
+var rows = table.rows.length; 
 var row = table.insertRow(rows);
 
 var cell1 = row.insertCell(0);
 var cell2 = row.insertCell(1);
 var cell3 = row.insertCell(2);
 
-cell1.innerHTML = "<a href=" + al + " target=\"_blank\">"+ an + "</a>";
-cell2.innerHTML = an; 
+cell1.innerHTML = an;
+cell2.innerHTML = "<div class=\"myDIV\"><a href=" + al + " target=\"_blank\">" + an + "</a></div> <div class=\"hide\">" + dd + " - " + ac + "</div>"; 
 cell3.innerHTML = an; 
 }
-
